341a5e86de4b80adb99a2554c8b38ef19430f473
89neuron <sourceitopen@gmail.com>
2017-09-30 21:39:45 +0530

    able to save to titan db

--- src/main/java/com/neuron/services/UserService.java
+++ src/main/java/com/neuron/services/UserService.java
@@ -1,19 +1,41 @@
 package com.neuron.services;
 
+
 import com.neuron.domains.User;
-import com.tinkerpop.blueprints.Graph;
-import com.tinkerpop.blueprints.impls.tg.TinkerGraph;
-import com.tinkerpop.blueprints.impls.tg.TinkerGraphFactory;
-import org.springframework.stereotype.Service;
+import com.thinkaurelius.titan.core.TitanFactory;
+import com.thinkaurelius.titan.core.TitanGraph;
+import org.apache.commons.configuration.BaseConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@Service
-public class UserService {
+public class UserService implements AutoCloseable{
 
 
+    Logger logger = LoggerFactory.getLogger(UserService.class);
+    private TitanGraph titanGraph;
 
     public User saveUser(){
-        TinkerGraph tinkerGraph = TinkerGraphFactory.createTinkerGraph();
+
+        User user = new User();
+        user.setAge(28);
+        user.setName("Neuron");
+        BaseConfiguration baseConfiguration = new BaseConfiguration();
+        baseConfiguration.setProperty("storage.backend", "cassandrathrift");
+        baseConfiguration.setProperty("storage.hostname", "127.0.0.1");
+
+        titanGraph = TitanFactory.open(baseConfiguration);
+        titanGraph.addVertex(user);
+        logger.debug("about to save user");
+        titanGraph.newTransaction().commit();
+//        GraphTraversalSource graphTraversalSource = titanGraph.traversal();
+//        Vertex v1 = graphTraversalSource.V().has("name","Arvind").next();
+//        System.out.println("savedd vertex now has id"+v1.property("age"));
+
         return null;
     }
 
+    @Override
+    public void close() throws Exception {
+        titanGraph.close();
+    }
 }
